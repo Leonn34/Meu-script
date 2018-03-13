031e87dde9c39ac76e55d9d33005d94f0b132752
Leonn34 <eltonsousadesigner@gmail.com>
2018-03-12 21:16:05 -0400

    update menu ssl

--- stunnel.sh
+++ stunnel.sh
@@ -5,15 +5,16 @@
 # RODANDO NA PORTA 443 
 
 clear
-echo "INSTALANDO STUNNEL"
-sleep 2
+echo "================ INSTALANDO STUNNEL ================"
+sleep 3
 apt-get update && apt-get upgrade -y
 apt-get install stunnel4 -y
+sleep 3
 clear
 
 # CONFIGURANDO
-echo "CONFIGURANDO STUNNEL..."
-sleep 2
+echo "============== CONFIGURANDO STUNNEL... =============="
+sleep 3
 echo "cert = /etc/stunnel/stunnel.pem" >> /etc/stunnel/stunnel.conf
 echo "socket = a:SO_REUSEADDR=1" >> /etc/stunnel/stunnel.conf
 echo "socket = l:TCP_NODELAY=1" >> /etc/stunnel/stunnel.conf
@@ -23,16 +24,16 @@ echo "[ssh]" >> /etc/stunnel/stunnel.conf
 echo "connect = 127.0.0.1:22" >> /etc/stunnel/stunnel.conf
 echo "accept = 443" >> /etc/stunnel/stunnel.conf
 clear
-echo "CONFIGURADO COM SUCESSO!"
+echo "============== CONFIGURADO COM SUCESSO! =============="
 sleep 3
 clear
 
 # CRIANDO CERTIFICADOS
-echo "GERANDO CERTIFICADOS SSL..."
-sleep 2
+echo "============= GERANDO CERTIFICADOS SSL... ============="
+sleep 3
 openssl genrsa -out key.pem 2048
-echo "PREENCHA OS DADOS (OPCIONAL)"
-sleep 2
+echo "============= PREENCHA OS DADOS (OPCIONAL) ============="
+sleep 3
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 
@@ -57,12 +58,13 @@ grep -v "^Port 443" /etc/ssh/sshd_config > /tmp/ssh && mv /tmp/ssh /etc/ssh/sshd
 echo "#Port 443" >> /etc/ssh/sshd_config
 
 clear
-echo "CERTIFICADOS GERADOS, REINICIANDO SERVIÇOS..."
-sleep 2
+echo "==== CERTIFICADOS GERADOS, REINICIANDO SERVIÇOS... ===="
+sleep 4
 service stunnel4 restart
 service ssh restart
 clear
 service stunnel4 restart
-echo "STUNNEL ISTALADO COM SUCESSO!"
+sleep 3
+echo "============ STUNNEL ISTALADO COM SUCESSO! ============="
 sleep 5
 clear
