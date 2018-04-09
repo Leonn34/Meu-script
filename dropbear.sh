c328fa2063403bce47b3235d7893b8a819bba3b4
Leonn34 <eltonsousadesigner@gmail.com>
2018-04-09 12:22:00 -0400

    update

--- dropbear.sh
+++ dropbear.sh
@@ -1,10 +1,68 @@
 #!/bin/bash
+####################################
+#CORES:
+#ESTILOS:
+#00: Nenhum
+#01: Negrito
+#04: Sublinhado
+#05: Piscante
+#07: Reverso
+#08: Oculto
+####################################
+#CORES TEXTO:
+#30: preto
+#31: Vermelho
+#32: verde
+#33: amarelo
+#34: Azul
+#35: Magenta (Rosa)
+#36: Ciano (Azul Ciano)
+#37: branco
+####################################
+#CORES FUNDO:
+#40: pretoo
+#41: Vermelho
+#42: verde
+#43: Amarelo
+#44: Azul
+#45: Magenta (Rosa)
+#46: Ciano (Azul Ciano)
+#47: Branco
+###############EXEMPLO:#############
+#Viva o Linux
+#echo -e '\033[01;37mViva o \033[04;32mLinux\033[00;37m!!!'
+#############################################
+##CORES DE FONTES,###################
+##INSERIR A COR NO INÍCIO E FINALIZA COM FIM
+##EXEMPLO:
+## echo -e $amarelo"SEU_TEXTO_AQUI"$fim
+#############################################
+corPadrao="\033[0m"
+preto="\033[0;30m"
+vermelho="\033[0;31m"
+verde="\033[0;32m"
+marrom="\033[0;33m"
+azul="\033[0;34m"
+purple="\033[0;35m"
+cyan="\033[0;36m"
+cinzaClaro="\033[0;37m"
+pretoCinza="\033[1;30m"
+vermelhoClaro="\033[1;31m"
+verdeClaro="\033[1;32m"
+amarelo="\033[1;33m"
+azulClaro="\033[1;34m"
+purpleClaro="\033[1;35m"
+cyanClaro="\033[1;36m"
+branco="\033[1;37m"
+fim="\033[0;37m"
+#############################################
 clear
-echo "MENU DROPBEAR"
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $branco" MENU DROPBEAR"$fim
 echo ""
-echo "[1] ATIVAR"
-echo "[2] DESATIVAR"
-echo "[3] SAIR"
+echo -e $amarelo" [1]"$fim $branco"ATIVAR"$fim
+echo -e $amarelo" [2]"$fim $branco"DESATIVAR"$fim
+echo -e $amarelo" [3]"$fim $branco"SAIR"$fim
 echo ""
 read -p " [1-3] " opcao
 echo " OPÇÃO ESCOLHIDA: " $opcao
