219f3981a454fb732cc190ecbc1f8b1b1b6ae520
Leonn34 <eltonsousadesigner@gmail.com>
2018-04-10 22:15:03 -0400

    update menu stunnel4

--- menu-stunnel4.sh
+++ menu-stunnel4.sh
@@ -1,38 +1,112 @@
 #!/bin/bash
+####################################
+#CORES:
+#ESTILOS:
+#00: Nenhum
+#01: Negrito
+#04: Sublinhado
+#05: Piscante
+#07: Reverso
+#08: Oculto
+####################################
+#CORES TEXTO:
+#30: preto
+#31: Vermelho
+#32: verde
+#33: amarelo
+#34: Azul
+#35: Magenta (Rosa)
+#36: Ciano (Azul Ciano)
+#37: branco
+####################################
+#CORES FUNDO:
+#40: pretoo
+#41: Vermelho
+#42: verde
+#43: Amarelo
+#44: Azul
+#45: Magenta (Rosa)
+#46: Ciano (Azul Ciano)
+#47: Branco
+###############EXEMPLO:#############
+#Viva o Linux
+#echo -e '\033[01;37mViva o \033[04;32mLinux\033[00;37m!!!'
+#############################################
+##CORES DE FONTES,###################
+##INSERIR A COR NO INÍCIO E FINALIZA COM FIM
+##EXEMPLO:
+## echo -e $amarelo"SEU_TEXTO_AQUI"$fim
+#############################################
+corPadrao="\033[0m"
+preto="\033[0;30m"
+vermelho="\033[0;31m"
+verde="\033[0;32m"
+marrom="\033[0;33m"
+azul="\033[0;34m"
+purple="\033[0;35m"
+cyan="\033[0;36m"
+cinzaClaro="\033[0;37m"
+pretoCinza="\033[1;30m"
+vermelhoClaro="\033[1;31m"
+verdeClaro="\033[1;32m"
+amarelo="\033[1;33m"
+azulClaro="\033[1;34m"
+purpleClaro="\033[1;35m"
+cyanClaro="\033[1;36m"
+branco="\033[1;37m"
+fim="\033[0;37m"
+#############################################
 clear
-echo "MENU STUNNEL4"
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $branco" MENU STUNNEL4"$fim
 echo ""
-echo "[1] ATIVAR"
-echo "[2] DESATIVAR"
-echo "[3] SAIR"
+echo -e $amarelo" [1]"$fim $branco"ATIVAR"$fim
+echo -e $amarelo" [2]"$fim $branco"DESATIVAR"$fim
+echo -e $amarelo" [3]"$fim $branco"SAIR"$fim
 echo ""
-read -p " [1-3] " opcao
-echo " OPÇÃO ESCOLHIDA: " $opcao
+read -p " DIGITE SUA OPÇÃO: [1-3] " opcao
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $cinzaClaro" OPÇÃO ESCOLHIDA:"$fim $branco$opcao$fim
+
 case $opcao in
+
 1)
-echo "================ ATIVANDO STUNNEL ================"
+clear
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $amarelo" ATIVANDO STUNNEL..."$fim
+#condição verifica se o stunnel está instalado, se não tiver será instalado...
 if [ -d /etc/stunnel ] ; then
 true
-echo "CONFIGURANDO O SSH"
+clear
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $amarelo" CONFIGURANDO O SSH..."$fim
 sleep 4
+clear
 grep -v "^Port 443" /etc/ssh/sshd_config > /tmp/ssh && mv /tmp/ssh /etc/ssh/sshd_config
 echo "#Port 443" >> /etc/ssh/sshd_config
-echo "============== REINICIANDO SERVIÇOS =============="
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $amarelo" REINICIANDO SERVIÇOS..."$fim
 sleep 4
+clear
 service squid3 stop
 service ssh restart
 service stunnel4 stop
 service stunnel4 start
-echo "================ STUNNEL ATIVADO! ================"
+clear
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $amarelo"SUNNEL ATIVADO!..."$fim
 sleep 5
+clear
 else
 wget https://raw.githubusercontent.com/Leonn34/scripts/master/stunnel.sh && chmod +x stunnel.sh && ./stunnel.sh
 fi
 ;;
 2)
 clear
-echo "================ DESATIVANDO STUNNEL ================"
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $amarelo"DESATIVANDO STUNNEL4"$fim
 sleep 4
+clear
 # REATIVANDO A PORTA 443 NO SSH
 grep -v "^#Port 443" /etc/ssh/sshd_config > /tmp/ssh && mv /tmp/ssh /etc/ssh/sshd_config
 echo "Port 443" >> /etc/ssh/sshd_config
@@ -41,7 +115,9 @@ service ssh restart
 service squid3 start
 service stunnel4 stop
 sleep 5
-echo "========== STUNNEL DESATIVADO COM SUCESSO! =========="
+clear
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $amarelo" STUNNEL DESATIVADO COM SUCESSO!"$fim
 sleep 5
 clear
 menu2
@@ -50,7 +126,7 @@ menu2
 menu2
 ;;
 *)
-echo "OPÇÃO INVÁLIDA!"
+echo -e $vermelhoClaro"OPÇÃO INVÁLIDA!!!"$fim
 sleep 3
 menu2
 exit
